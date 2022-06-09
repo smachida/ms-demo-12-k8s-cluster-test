fcdce7861cff7d98d248663142d15d4ea737a4d1
Shuichi Machida <shuichi.machida@gmail.com>
2022-06-09 22:18:32 +0900

    updated

--- minikube/00-minikube-create.sh
+++ minikube/00-minikube-create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 unset KUBECONFIG
 
-minikube start --memory=10240 --cpus=4 --disk-size=30g --kubernetes-version=v1.22.10 --vm-driver=virtualbox
+minikube start --kubernetes-version=v1.22.10
 
 minikube addons enable ingress
 minikube addons enable metrics-server
